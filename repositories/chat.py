126970c81cfdd4dc8a46669b1c0af5ea135ab02f
ahmetsenturk <iahmetsenturk@yahoo.com>
2025-01-13 01:06:11 +0100

    add repositories

---
+++ repositories/chat.py
@@ -0,0 +1,30 @@
+from typing import List
+import datetime
+
+from repositories.base import MongoRepository
+from schemas.chat_schema import ChatMessage, UserSession
+
+
+class SessionRepository(MongoRepository[UserSession]):
+    def __init__(self):
+        super().__init__(UserSession, "sessions")
+    
+    async def get_or_create_session(self, whatsapp_id: str) -> UserSession:
+        session = await self.get_by_field("whatsapp_id", whatsapp_id)
+        if not session:
+            session = await self.create(UserSession(whatsapp_id=whatsapp_id))
+        else:
+            await self.update(str(session.id), {"last_active": datetime.utcnow()})
+        return session
+
+class MessageRepository(MongoRepository[ChatMessage]):
+    def __init__(self):
+        super().__init__(ChatMessage, "messages")
+    
+    async def get_recent_messages(self, whatsapp_id: str, limit: int = 10) -> List[ChatMessage]:
+        return await self.list(
+            filter_query={"whatsapp_id": whatsapp_id},
+            sort_by="created_at",
+            sort_desc=True,
+            limit=limit
+        )
--- utils/constants.py
+++ utils/constants.py
@@ -1,1 +1,12 @@
 CHAT_HISTORY_LIMIT = 10
+
+# RAG config, could be explicitely given by a config file as well
+RAG_CONFIG = {
+    "db_path": "utils/db/",
+    "embedding_model": "text-embedding-ada-002",
+    "api_version": "2023-05-15",
+    "retriever_args": {
+        "search_type":"mmr", 
+        "search_kwargs": {'k': 5, 'fetch_k': 100, "score_threshold": 0.75}},
+    "prompt_path": "utils/prompts/"
+}
